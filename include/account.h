86d77581b37195dfdaa057f2cfd62fd9df3ce1ec
RAGREON <ragreon809@gmail.com>
2025-08-31 18:59:55 +0545

    chore: remove account.h

--- include/account.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct {
-  char[10]    customer_id;
-  int         balance;
-} Account;
-
-void create_account(char[10] customer_id, int balance);
-void delete_account(char[10] customer_id);
