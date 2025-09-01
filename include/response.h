05d9a1628fe355f0ee66240b29ca50bb2ae06af1
RAGREON <ragreon809@gmail.com>
2025-09-01 20:56:56 +0545

    feat: add create_http_response

--- include/response.h
+++ include/response.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 typedef enum {
   OK = 200, 
   NOT_FOUND = 404
@@ -9,3 +13,5 @@ typedef struct {
   StatusCode    status_code; 
   char          data[50];
 } Response;
+
+char* create_http_response(Response* response);
